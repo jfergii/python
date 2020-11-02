3ac6c6e1b94e9632c669d427e76ea2ccb01a3d00
JustinF <jusfer95@hotmail.com>
2020-11-02 09:59:50 -0800

    refactored to only have functions, and removed unnecessary comments

--- guessTheNumberFunctions.py
+++ guessTheNumberFunctions.py
@@ -1,88 +1,94 @@
+#imports for function
+import random
+import sys
 
-#main function - is this the preferred way in python to declare main?
-def Main():
+#control variables 
+attemptNumLimit = int(5)
+attemptNum = int(1)
 
-    #imports for function
-    import random
-    import sys
+#sys_main function - is this the preferred way in python to declare sys_main?
+def sys_main():
 
-    def sysexit():
+    def sys_exit():
         print("Game is exiting.")
         sys.exit()
 
-    #function to accept user input
-    def userInput():
+    def user_input():
         return input("Type here:")
         
-    def playGame():
+    def check_attempts(attemptNum):
+        if attemptNum != attemptNumLimit:
+            print("You have tried", attemptNum, "out of", attemptNumLimit, "times.")
+            attemptNum = attemptNum + 1
+            return (attemptNum)
+        elif attemptNum == attemptNumLimit:
+            print("You're out of attempts. Try again next time.")
+            sys_exit()
 
+    def check_guess(numGuess, numberToGuess):
+        if numGuess > numberToGuess:
+            print(numGuess, "is higher than the number.")
+            return(False)
+        elif numGuess < numberToGuess:
+            print(numGuess, "is lower than the number.")
+            return(False)
+        elif numGuess == numberToGuess:
+            print("Success! Number to guess was:", numberToGuess)
+            sys_exit()
+
+    def get_upper_limit():
+        print("Please enter a high number in the range you wish to guess from.")
+        upperNumber = user_input()
+        upperNumber = int(upperNumber)
+        return(upperNumber)
+
+    def get_lower_limit():
+        print("Please enter a lower number in the range you wish to guess from.")
+        lowerNumber = user_input()
+        lowerNumber = int(lowerNumber)
+        return(lowerNumber)
+
+    def generate_randNum():
+        lowerNumber = get_lower_limit()
+        upperNumber = get_upper_limit()
+        numberToGuess = random.randint(lowerNumber, upperNumber)
+        numberToGuess = int(numberToGuess)
+        return numberToGuess
+
+    def get_guess(): 
+        print("What number would you like to guess first?")
+        numGuess = user_input()
+        numGuess = int(numGuess)
+        return numGuess
+
+    def play_game():
         print("Would you like to play the number game? yes/no")
-        yesNo = userInput()
+        yesNo = user_input()
 
-       #beginning logic for starting the game
         if yesNo == "yes":
             print("Great! Lets continue...")
+            return(True)
         elif yesNo == "no":
             print("Maybe next time!")
-            sysexit()
+            sys_exit()
         else:
             print("input unexpected.")
             print(yesNo)
-            sysexit()
+            sys_exit()
 
-        #gather lower limit number
-        print("Please enter a lower number in the range you wish to guess from.")
-        lowerNumber = userInput()
-        lowerNumber = int(lowerNumber) #convert to int - maybe not necessary
-
-        #gather upper limit number
-        print("Please enter a high number in the range you wish to guess from.")
-        upperNumber = userInput()
-        upperNumber = int(upperNumber) #convert to int - maybe not necessary
-
-        #prints range for user and generates random number
-        print("Your range is: ", lowerNumber, " to ", upperNumber)
-        numberToGuess = random.randint(lowerNumber, upperNumber)
-        numberToGuess = int(numberToGuess)
-
-        #attempt number control variables 
+######################################################################################
+#being running functions
+    if play_game() == True :
         attemptNum = int(1)
-        attemptNumLimit = int(5)
-
-        #guess variable is populated based off first input
+        numberToGuess = generate_randNum()
         print("You get ", attemptNumLimit, " guesses. Good Luck!")
-        print("What number would you like to guess first?")
-        guess = userInput()
-        guess = int(guess)
 
-        #while loop for attempt limit - not sure if this could be done better
         while attemptNum != attemptNumLimit:
-            if guess > numberToGuess:
-                print("The number is lower than ", guess)
-                guess = input("What number would you like to guess?: ")
-                guess = int(guess)
-                attemptNum = attemptNum + 1
-            elif guess < numberToGuess:
-                print("The number is highter than ", guess)
-                guess = input("What number would you like to guess?: ")
-                guess = int(guess)
-                attemptNum = attemptNum + 1
-            elif guess == numberToGuess:
-                print("Success! Number to guess was:", numberToGuess)
-                print("You've won the game!")
-                print("Congrats!")
-                print("See you next time!")
-                sysexit() 
+            numGuess = get_guess()
+            check_guess(numGuess, numberToGuess)
+            attemptNum = check_attempts(attemptNum)
 
-        if attemptNum == attemptNumLimit:
-            print("You've tried too many times. You lose.")
-            print("The number was:", numberToGuess)
-            sysexit()
-
-    #run playGame Function
-    playGame()
-
-#run Main
-Main()
+#run sys_main
+sys_main()
 
 
